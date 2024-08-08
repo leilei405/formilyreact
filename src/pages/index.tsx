144779a068b426405c99aa5cf67780c973720480
leilei405 <1601178425@qq.com>
2024-08-09 00:03:16 +0800

    feat: 使用antd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,12 @@ import 'antd/dist/antd.css';
 import { Link } from 'umi';
 //注入antd的样式
 import Com from './field/voidField';
+import { Input } from 'antd';
 
 function IndexPage() {
     return (
         <div>
+            <Input />
             <h1 className={styles.title}>主页</h1>
             <Com />
         </div>
